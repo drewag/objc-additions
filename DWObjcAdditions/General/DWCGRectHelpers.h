a2aa16a64caa7b7f78c456ee86559b6568d23342
Andrew Wagner <andrew@drewag.me>
2014-03-10 11:04:41 -0600

    [FC] Added new CGRect helper to pin to botton of another rect

--- DWObjcAdditions/General/DWCGRectHelpers.h
+++ DWObjcAdditions/General/DWCGRectHelpers.h
@@ -6,38 +6,38 @@
 //  Copyright (c) 2013 Drewag LLC. All rights reserved.
 //
 
-static inline CGRect CGRectResize(CGRect rect, CGFloat deltaWidth, CGFloat deltaHeight) {
+static inline CGRect DWCGRectResize(CGRect rect, CGFloat deltaWidth, CGFloat deltaHeight) {
 	return CGRectMake(rect.origin.x, rect.origin.y, rect.size.width + deltaWidth, rect.size.height + deltaHeight);
 }
 
-static inline CGRect CGRectPosition(CGRect rect, CGFloat originX, CGFloat originY) {
+static inline CGRect DWCGRectPosition(CGRect rect, CGFloat originX, CGFloat originY) {
 	return CGRectMake(originX, originY, rect.size.width, rect.size.height);
 }
 
-static inline CGRect CGRectFromSize(CGSize size) {
+static inline CGRect DWCGRectFromSize(CGSize size) {
     return CGRectMake(0, 0, size.width, size.height);
 }
 
-static inline CGRect CGRectPositionSize(CGSize size, CGFloat originX, CGFloat originY) {
+static inline CGRect DWCGRectPositionSize(CGSize size, CGFloat originX, CGFloat originY) {
     return CGRectMake(originX, originY, size.width, size.height);
 }
 
-static inline CGFloat CGRectBottom(CGRect rect) {
+static inline CGFloat DWCGRectBottom(CGRect rect) {
     return rect.origin.y + rect.size.height;
 }
 
-static inline CGFloat CGRectRight(CGRect rect) {
+static inline CGFloat DWCGRectRight(CGRect rect) {
     return rect.origin.x + rect.size.width;
 }
 
-static inline CGPoint CGRectCenter(CGRect rect) {
+static inline CGPoint DWCGRectCenter(CGRect rect) {
     return CGPointMake(
         rect.origin.x + rect.size.width / 2.0,
         rect.origin.y + rect.size.height / 2.0
     );
 }
 
-static CGSize CGSizeAspectFit(CGSize aspectRatio, CGSize boundingSize) {
+static inline CGSize DWCGSizeAspectFit(CGSize aspectRatio, CGSize boundingSize) {
     float mW = boundingSize.width / aspectRatio.width;
     float mH = boundingSize.height / aspectRatio.height;
     if( mH < mW )
@@ -45,4 +45,13 @@ static CGSize CGSizeAspectFit(CGSize aspectRatio, CGSize boundingSize) {
     else if( mW < mH )
         boundingSize.height = boundingSize.width / aspectRatio.width * aspectRatio.height;
     return boundingSize;
+}
+
+static inline CGRect DWCGRectPinToBottomWithOffset(CGRect rect, CGFloat yOffset, CGFloat width, CGFloat height) {
+    return CGRectMake(
+        rect.origin.x,
+        DWCGRectBottom(rect) + yOffset,
+        width,
+        height
+    );
 }
